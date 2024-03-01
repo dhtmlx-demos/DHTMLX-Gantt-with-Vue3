52029e60d7d2aa02b5be8eb07a7ab098afda196f
DHTMLX <155955527+dhtmlx1@users.noreply.github.com>
2024-03-01 03:12:56 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Gantt from "./components/Gantt.vue";
+import { getData } from "./data";
+
+export default {
+  components: { Gantt },
+  data() {
+    return {
+      tasks: getData(),
+    };
+  },
+};
+</script>
+
+<template>
+  <Gantt :tasks="tasks" />
+</template>
---
+++ src/components/Gantt.vue
@@ -0,0 +1,24 @@
+<script>
+import { Gantt } from "@dhx/trial-gantt";
+import "@dhx/trial-gantt/codebase/dhtmlxgantt.css";
+
+export default {
+  props: ["tasks"],
+
+  mounted() {
+    let gantt = Gantt.getGanttInstance();
+    gantt.init(this.$refs.cont);
+    gantt.parse(this.tasks);
+
+    this.gantt = gantt;
+  },
+  unmounted() {
+    this.gantt.destructor();
+    this.$refs.cont.innerHTML = "";
+  },
+};
+</script>
+
+<template>
+  <div ref="cont" style="width: 100%; height: 100%"></div>
+</template>
